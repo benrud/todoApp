414f423d488a998fb7b7a86d2261c074c0474abd
Todd Benrud <benrud@users.noreply.github.com>
2023-01-30 18:27:50 +0000

    Connect form

--- functions.php
+++ functions.php
@@ -1,58 +1,61 @@
 <?php session_start();
+$taskDataFile = $_SERVER['DOCUMENT_ROOT']."/data/tasks.json";
+$usersDataFile = $_SERVER['DOCUMENT_ROOT']."/data/users.json";
+
+// JSON data needed for app.
+    // Get data from json file.
+    $jsonTasks = file_get_contents($taskDataFile); 
+    $jsonUsers = file_get_contents($usersDataFile); 
+    // Convert json data to PHP array.
+    $tasksData =  json_decode($jsonTasks, TRUE);   
+    $usersData =  json_decode($jsonUsers, TRUE);
+// JSON data needed for app.
+
+// **************** reusable DATA manipulation functions and variables below
+// **************** reusable DATA manipulation functions and variables below
+// **************** reusable DATA manipulation functions and variables below
 
 
+// **************** reusable TEMPLATE variables below
+// **************** reusable TEMPLATE variables below
+// **************** reusable TEMPLATE variables below
+  $styleSheetFiles = 
+    ' 
+      <!-- Favicon-->
+      <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      <!-- Bootstrap icons-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+      <!-- Core theme CSS (includes Bootstrap)-->
+      <link href="/resources/templateFiles/css/styles.css" rel="stylesheet" />
+      <link href="/resources/style.css" rel="stylesheet" />
+    ';
+
+  $javaScriptFiles = 
+    ' 
+      <!-- Bootstrap core JS-->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+      <!-- Core theme JS-->
+      <script src="/resources/templateFiles/js/scripts.js"></script>
+      <script src="/resources/script.js"></script>
+    ';
 
 
-
-
-
-
-
-
-
-
-
-// ************* Template variables are below 
-// ************* Template variables are below 
-
-$cssFiles =
-  '  
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="/resources/templateFiles/css/styles.css" rel="stylesheet" />
+$navBar = 
+  '
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-lg-5">
+            <a class="navbar-brand" href="/index.php">Todd\'s Task Manager</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="/index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/tasks/">Tasks</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/tasks/new.php">New Task</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/tasks/status.php">Search</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
   ';
 
-$jsFiles =
-  '  
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="/resources/templateFiles/js/scripts.js"></script>
-';
-
-$nav =
-'  
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container px-lg-5">
-        <a class="navbar-brand" href="#!">Todd\'s ToDo Manager</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Tasks</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Search</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-';
-
-
-$footer =
-'  
-<div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
-';
 ?>
---
+++ redirects/task.php
@@ -0,0 +1,50 @@
+<?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
+
+//$tasksData and $usersData are defined in functions.php.
+
+// #1 MAKE SURE FORM DATA IS VALID
+// process form IF user selected and BTN_create was clicked 
+if(isset($_POST['BTN_create']) && $_POST['user'] > 0) {
+
+ $date = date_create($_POST['dateDeadline']);
+ $deadLineReformatted = date_format($date,"Y/m/d");
+  
+  //Create a new LARGEST UID based on existing UIDs in an array. 
+  $largest_uid = 0;
+  foreach ($tasksData as $item) {
+    if ($item['uid'] > $largest_uid) {
+        $largest_uid = $item['uid'];
+        $newUID = $largest_uid+1;
+    }
+  } 
+  
+  $newFormData = array(
+    "uid"=> "$newUID", 
+    "userUID"=> $_POST['user'], 
+    "dateCreate"=> date("Y/m/d"), 
+    "dateDeadline"=> "$deadLineReformatted", 
+    "dateComplete"=> NULL, 
+    "title"=> $_POST['title'],
+    "description"=> $_POST['description'],
+    "status"=> "created", 
+    "reward"=> $_POST['reward'],
+    "timeNeeded"=> $_POST['timeNeeded'],
+    "category"=> $_POST['category']
+  );
+  
+
+  array_push($tasksData, $newFormData);
+  $tasksDataAsJSON = json_encode($tasksData, JSON_PRETTY_PRINT);
+  file_put_contents($taskDataFile, $tasksDataAsJSON);
+
+
+  
+  echo '<pre>';
+  var_dump($tasksData);
+  echo '</pre>';
+
+  
+  }
+
+//   header('Location: /index.php');
+?>
