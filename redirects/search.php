62a673fe500bde20063c07f59053451b3190c864
Todd Benrud <benrud@users.noreply.github.com>
2023-02-15 22:37:46 +0000

    filter logic update

--- redirects/search.php
+++ redirects/search.php
@@ -1,19 +1,44 @@
 <?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
+
+
+  $matchCategory = ($age >= 18) ? true : false;
+
+  if (isset($_POST['BTN_search'])) { // START:: IF BUTTON pressed.
+
+      if ($_POST['filterCategory'] != "1" && $_POST['filterUser'] != "1") { // START:: IF all filters used.
+          $i = 0;
+          foreach($tasksData as $task){
+            if($task['category'] == $_POST['filterCategory'] && $task['userUID'] == $_POST['filterUser'] ) {
+              $_SESSION['search'][$i] = $task;
+              $i++;
+            } // END IF
+          } // END FOREACH
   
-  if (isset($_POST['BTN_search']) && $_POST['filterCategory'] != "1" && $_POST['filterUser'] != "1") {
+      // End:: IF all filters used.
+      // if user ONLY                                                                     
+      } elseif ($_POST['filterCategory'] == "1" && $_POST['filterUser'] != "1") {
+          $i = 0;
+          foreach($tasksData as $task){
+            
+            if($task['userUID'] == $_POST['filterUser']) {
+              $_SESSION['search'][$i] = $task;
+              $i++;
+            } // END IF
+            
+          } // END FOREACH
 
-      $i = 0;
-      foreach($tasksData as $task){
-        
-        if($task['category'] == $_POST['filterCategory']) {
-          $_SESSION['search'][$i] = $task;
-          $i++;
-        } // END IF
-        
-      } // END FOREACH
 
-      header('Location: /tasks/index.php?filter=1');
-  } 
+        
+      }
+    
+    
+    
+    
+    
+    
+    
+    header('Location: /tasks/index.php?filter=1');
+  }// END:: IF BUTTON pressed. 
 
     // echo '<pre>';
     // var_dump($_SESSION);
